b70a06842954b62c39f763853ae28f18c897ed4d
Vladislav Zubakin <shaqimusic@gmail.com>
2022-06-28 20:43:48 +0300

    feat: remove generic type

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,6 +1,6 @@
 import Loader from './loader';
 
-class AppLoader<T> extends Loader<T> {
+export class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
             apiKey: '74fc0dd205c64b0db8cc1f137d375a0b',
