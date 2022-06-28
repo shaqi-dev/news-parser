c42420f49b8f43dac564d95d6e97fbe574769052
Vladislav Zubakin <shaqimusic@gmail.com>
2022-06-28 17:57:52 +0300

    feat: add generic type to appLoader

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,6 +1,6 @@
 import Loader from './loader';
 
-class AppLoader extends Loader {
+class AppLoader<T> extends Loader<T> {
     constructor() {
         super('https://newsapi.org/v2/', {
             apiKey: '74fc0dd205c64b0db8cc1f137d375a0b',
