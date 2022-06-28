4dcdd4ac03f5dd9bc8da9489c5ef057ac5cdcbca
Vladislav Zubakin <shaqimusic@gmail.com>
2022-06-28 16:52:18 +0300

    refactor: delete unnecessary exclamations

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -17,35 +17,35 @@ class News implements NewsDrawer {
             const newsClone = newsItemTemp.content.cloneNode(true) as HTMLDivElement;
 
             if (idx % 2) {
-                const newsItem = newsClone.querySelector('.news__item')! as HTMLDivElement;
+                const newsItem = newsClone.querySelector('.news__item') as HTMLDivElement;
                 newsItem.classList.add('alt');
             }
 
-            const metaPhoto = newsClone.querySelector('.news__meta-photo')! as HTMLDivElement;
+            const metaPhoto = newsClone.querySelector('.news__meta-photo') as HTMLDivElement;
             metaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
 
-            const metaAuthor = newsClone.querySelector('.news__meta-author')! as HTMLLIElement;
+            const metaAuthor = newsClone.querySelector('.news__meta-author') as HTMLLIElement;
             metaAuthor.textContent = item.author || item.source.name;
 
-            const metaDate = newsClone.querySelector('.news__meta-date')! as HTMLLIElement;
+            const metaDate = newsClone.querySelector('.news__meta-date') as HTMLLIElement;
             metaDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
 
-            const descrTitle = newsClone.querySelector('.news__description-title')! as HTMLHeadingElement;
+            const descrTitle = newsClone.querySelector('.news__description-title') as HTMLHeadingElement;
             descrTitle.textContent = item.title;
 
-            const descrSource = newsClone.querySelector('.news__description-source')! as HTMLHeadingElement;
+            const descrSource = newsClone.querySelector('.news__description-source') as HTMLHeadingElement;
             descrSource.textContent = item.source.name;
 
-            const descrContent = newsClone.querySelector('.news__description-content')! as HTMLParagraphElement;
+            const descrContent = newsClone.querySelector('.news__description-content') as HTMLParagraphElement;
             descrContent.textContent = item.description;
 
-            const readMore = newsClone.querySelector('.news__read-more a')! as HTMLAnchorElement;
+            const readMore = newsClone.querySelector('.news__read-more a') as HTMLAnchorElement;
             readMore.setAttribute('href', item.url);
 
             fragment.append(newsClone);
         });
 
-        const newsElement = document.querySelector('.news')! as HTMLDivElement;
+        const newsElement = document.querySelector('.news') as HTMLDivElement;
         newsElement.innerHTML = '';
         newsElement.appendChild(fragment);
     }
