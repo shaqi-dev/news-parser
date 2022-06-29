b542c7953ee3d733de30770bddff93c8d12791b5
Vladislav Zubakin <shaqimusic@gmail.com>
2022-06-29 21:03:06 +0300

    fix: api key

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 export class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '74fc0dd205c64b0db8cc1f137d375a0b',
+            apiKey: 'cba7e90412ea4943b8c3f87f2503e841',
         });
     }
 }
